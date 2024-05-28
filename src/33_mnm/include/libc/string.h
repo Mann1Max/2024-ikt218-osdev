065fe1229356d03c16bee696ea3120a6943cb004
Maxh <maxh@uia.no>
2024-05-28 09:34:00 +0000

    Apply changes from evenmoretest to src/33_mnm

--- src/33_mnm/include/libc/string.h
+++ src/33_mnm/include/libc/string.h
@@ -2,3 +2,5 @@
 #include "./stdint.h"
 
 size_t strlen(const char* str);
+
+void int_to_string(int value, char* buffer);
--- src/33_mnm/src/kernel.cpp
+++ src/33_mnm/src/kernel.cpp
@@ -1,6 +1,10 @@
-
-
 extern "C" int kernel_main(void);
+
 int kernel_main(){
+
+
+
+
+    while(true){};
     return 0;
 }
